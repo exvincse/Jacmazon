b4b29a7fc393c8d57cbd270262490f4e2eafab2b
Jack <suzijie860706@gmail.com>
2024-07-06 14:36:28 +0800

    change apiUrl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5092/api/webapi',
+  //apiUrl: 'http://localhost:5092/api/webapi',
+  apiUrl: 'http://192.168.18.7:8012/api/webapi',
   apiKey: '',
   imgUrl: '',
 };
